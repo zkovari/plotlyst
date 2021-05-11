49c53fe297b1017469520ce0a6c7579cb356189c
zkovari <zsolt@kovaridev.com>
2021-05-11 21:13:10 +0200

    Draw avatar in timeline

--- novel_outliner/view/timeline_view.py
+++ novel_outliner/view/timeline_view.py
@@ -1,11 +1,10 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QHeaderView, QGraphicsScene, QFrame
+from PyQt5.QtWidgets import QWidget, QGraphicsScene, QFrame
 
 from novel_outliner.core.domain import Novel, Scene
-from novel_outliner.model.scenes_model import ScenesTableModel
-from novel_outliner.model.timeline_model import TimelineScenesFilterProxyModel
 from novel_outliner.view.generated.scene_card_widget_ui import Ui_SceneCardWidget
 from novel_outliner.view.generated.timeline_view_ui import Ui_TimelineView
+from novel_outliner.view.icons import avatars
 
 
 class TimelineView:
@@ -16,29 +15,9 @@ class TimelineView:
         self.ui.setupUi(self.widget)
         self.novel = novel
 
-        self.model = ScenesTableModel(self.novel)
-        self._proxy = TimelineScenesFilterProxyModel()
-        self._proxy.setSourceModel(self.model)
-        self._proxy.setSortCaseSensitivity(Qt.CaseInsensitive)
-        self._proxy.setFilterCaseSensitivity(Qt.CaseInsensitive)
-        self._proxy.sort(ScenesTableModel.ColTime, Qt.AscendingOrder)
-        self.ui.tblTimelineScenes.setModel(self._proxy)
-
-        self.ui.tblTimelineScenes.setColumnWidth(ScenesTableModel.ColTitle, 400)
-        self.ui.tblTimelineScenes.horizontalHeader().setSectionResizeMode(ScenesTableModel.ColCharacters,
-                                                                          QHeaderView.ResizeToContents)
-        self.ui.tblTimelineScenes.hideColumn(ScenesTableModel.ColType)
-        self.ui.tblTimelineScenes.horizontalHeader().swapSections(ScenesTableModel.ColPov, ScenesTableModel.ColTime)
-        self.ui.tblTimelineScenes.horizontalHeader().swapSections(ScenesTableModel.ColTime,
-                                                                  ScenesTableModel.ColCharacters)
         self.refresh()
 
     def refresh(self):
-        # for row in range(self._proxy.rowCount()):
-        #     self.ui.tblTimelineScenes.setIndexWidget(self._proxy.index(row, ScenesTableModel.ColCharacters),
-        #                                              SceneCharactersWidget(
-        #                                                  self._proxy.index(row, 0).data(ScenesTableModel.SceneRole)))
-
         scene = QGraphicsScene()
         scenes = [x for x in self.novel.scenes if x.day]
         scenes = sorted(scenes, key=lambda x: x.day)
@@ -65,16 +44,20 @@ class TimelineView:
             item.moveBy(scene_x, -500 + index * 80 + 30)
             item.setToolTip(s.synopsis)
 
-        self.ui.graphicsView.setScene(scene)
+        self.ui.graphicsTimeline.setScene(scene)
 
 
 class SceneCardWidget(QFrame, Ui_SceneCardWidget):
     def __init__(self, scene: Scene, parent=None):
         super(SceneCardWidget, self).__init__(parent)
         self.setupUi(self)
-
+        self.setFrameShape(QFrame.StyledPanel)
         self.lblTitle.setText(scene.title)
         self.resize(len(scene.title) * 12, 40)
+
+        if scene.pov:
+            self.lblPic.setPixmap(avatars.pixmap(scene.pov).scaled(32, 32, Qt.KeepAspectRatio,
+                                                                   Qt.SmoothTransformation))
 
         self.setMaximumHeight(80)
 
@@ -88,7 +71,7 @@ class SceneCardWidget(QFrame, Ui_SceneCardWidget):
             border = '2px'
         self.setStyleSheet(f'''QFrame {{border: {border} solid {color};
             border-radius: 12px;
-            background-color: white;
+            background-color: #9e87de;
             }}
             QLabel {{
             border: 0px;
