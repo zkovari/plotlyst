e60803cf998d4d3df842783f4cf9145d29fe67a7
zkovari <zsolt@kovaridev.com>
2021-06-25 17:42:22 +0200

    Set up fbs

--- novel_outliner/__main__.py
+++ novel_outliner/__main__.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import sys
 
-from PyQt5 import QtWidgets
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
 
 from novel_outliner.common import EXIT_CODE_RESTART
 from novel_outliner.event.handler import exception_handler
@@ -10,16 +10,15 @@ from novel_outliner.view.main_window import MainWindow
 from novel_outliner.view.stylesheet import APP_STYLESHEET
 
 if __name__ == '__main__':
+    appctxt = ApplicationContext()
     while True:
-        app = QtWidgets.QApplication.instance()
-        if not app:
-            app = QtWidgets.QApplication(sys.argv)
+        app = appctxt.app
         window = MainWindow()
         app.setStyleSheet(APP_STYLESHEET)
 
         window.show()
         sys.excepthook = exception_handler  # type: ignore
-        exit_code = app.exec()
+        exit_code = appctxt.app.exec_()
         if exit_code < EXIT_CODE_RESTART:
             break
 
