641e9933422f033b7798712d4c6d51bc2d09845f
zkovari <zsolt@kovaridev.com>
2021-07-09 18:10:42 +0200

    Add tree model

---
+++ novel_outliner/model/tree_model.py
@@ -0,0 +1,87 @@
+from typing import Any
+
+from PyQt5.QtCore import QAbstractItemModel, QModelIndex, QVariant, Qt
+from anytree import Node
+from overrides import overrides
+
+
+class TreeItemModel(QAbstractItemModel):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.root = Node('root')
+
+    def rootNode(self) -> Node:
+        return self.root
+
+    def setRootNode(self, node: Node):
+        self.root = node
+
+    @overrides
+    def index(self, row: int, column: int, parent: QModelIndex) -> QModelIndex:
+        if not self.hasIndex(row, column, parent):
+            return QModelIndex()
+
+        parent_node: Node
+        if not parent.isValid():
+            parent_node = self.root
+        else:
+            parent_node = parent.internalPointer()
+
+        if row >= len(parent_node.children):
+            return QModelIndex()
+
+        child_node = parent_node.children[row]
+        return self.createIndex(row, column, child_node)
+
+    @overrides
+    def parent(self, index: QModelIndex) -> QModelIndex:
+        if not index.isValid():
+            return QModelIndex()
+
+        child_node: Node = index.internalPointer()
+        parent_node: Node = child_node.parent
+        if not parent_node:
+            print(child_node.name)
+
+        if parent_node == self.root:
+            return QModelIndex()
+
+        if parent_node.parent == self.root or not parent_node.parent:
+            parent_node_index = 0
+        else:
+            parent_node_index = parent_node.parent.children.index(parent_node)
+
+        return self.createIndex(parent_node_index, 0, parent_node)
+
+    @overrides
+    def rowCount(self, parent: QModelIndex) -> int:
+        if parent.column() > 0:
+            return 0
+
+        parent_node: Node
+        if not parent.isValid():
+            parent_node = self.root
+        else:
+            parent_node = parent.internalPointer()
+        return len(parent_node.children)
+
+    @overrides
+    def columnCount(self, parent: QModelIndex) -> int:
+        if parent.isValid():
+            node: Node = parent.internalPointer()
+            return node.depth
+        return 1
+
+    @overrides
+    def data(self, index: QModelIndex, role: int = Qt.DisplayRole) -> Any:
+        if not index.isValid():
+            return QVariant()
+        if role == Qt.DisplayRole:
+            node: Node = index.internalPointer()
+            return node.name
+
+    @overrides
+    def flags(self, index: QModelIndex) -> Qt.ItemFlags:
+        if not index.isValid():
+            return Qt.NoItemFlags
+        return super().flags(index)
