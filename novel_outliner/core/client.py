f74bffd1b9f26f3016c4b531f8e602e3c607dc76
zkovari <zsolt@kovaridev.com>
2021-07-10 19:49:56 +0200

    Add copyright and license notice

--- novel_outliner/core/client.py
+++ novel_outliner/core/client.py
@@ -1,3 +1,22 @@
+"""
+Plotlyst
+Copyright (C) 2021  Zsolt Kovari
+
+This file is part of Plotlyst.
+
+Plotlyst is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Plotlyst is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import os
 from typing import List
 
@@ -165,7 +184,7 @@ class SqlClient:
                     if chapter.id == scene_m.chapter.id:
                         scene_chapter = chapter
                         break
-                    
+
             day = scene_m.day if scene_m.day else 0
             end_event = scene_m.end_event if scene_m.end_event else True
             scenes.append(Scene(id=scene_m.id, title=scene_m.title, synopsis=scene_m.synopsis, type=scene_m.type,
