873729ded13c01788d880d6e0d058d16f05a8714
zkovari <zsolt@kovaridev.com>
2021-07-21 15:49:07 +0200

    Wair for story maps to be painted in tests

--- src/main/python/plotlyst/test/view/test_reports.py
+++ src/main/python/plotlyst/test/view/test_reports.py
@@ -6,6 +6,8 @@ from src.main.python.plotlyst.view.reports_view import ReportsView
 def test_reports_display(qtbot, filled_window: MainWindow):
     view: ReportsView = go_to_reports(filled_window)
     assert view.ui.tabWidget.currentWidget() == view.ui.tabStoryMap
+
+    qtbot.wait(200)  # wait until painted
     view.ui.tabWidget.setCurrentWidget(view.ui.tabCharacters)
     view.ui.tabWidget_2.setCurrentWidget(view.ui.tabCharacterArcs)
     view.ui.tabWidget.setCurrentWidget(view.ui.tabStoryDistribution)
