5047560b760cda4fc39a3c9a98f3b7ccfc27672a
zkovari <zsolt@kovaridev.com>
2021-08-06 14:49:04 +0200

    Import scenes from scrivener in UI

--- src/main/python/plotlyst/test/view/test_main_window.py
+++ src/main/python/plotlyst/test/view/test_main_window.py
@@ -1,26 +1,42 @@
+import sys
+from pathlib import Path
+from uuid import UUID
+
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog
 
 from src.main.python.plotlyst.core.client import client
 from src.main.python.plotlyst.test.common import go_to_home, create_novel
-from src.main.python.plotlyst.test.conftest import get_main_window
 from src.main.python.plotlyst.view.home_view import HomeView
 from src.main.python.plotlyst.view.main_window import MainWindow
 
 
-def test_empty_window(qtbot, test_client):
-    assert not client.novels()
+def assert_views(window: MainWindow, visible: bool = True, scene_dependent_enabled: bool = True,
+                 character_dependent_enabled: bool = True):
+    assert window.btnNovel.isVisible() == visible
+    assert window.btnCharacters.isVisible() == visible
+    assert window.btnScenes.isVisible() == visible
+    assert window.btnReport.isVisible() == visible
+    assert window.btnNotes.isVisible() == visible
+    assert window.btnTimeline.isVisible() == visible
 
-    window = get_main_window(qtbot)
+    if not visible:
+        return
+
+    assert window.btnNovel.isEnabled()
+    assert window.btnCharacters.isEnabled()
+    assert window.btnScenes.isEnabled() == character_dependent_enabled
+    assert window.btnReport.isEnabled() == scene_dependent_enabled
+    assert window.btnNotes.isEnabled() == scene_dependent_enabled
+    assert window.btnTimeline.isEnabled() == scene_dependent_enabled
+
+
+def test_empty_window(qtbot, window: MainWindow):
+    assert not client.novels()
 
     assert window.btnHome.isVisible()
     assert window.btnHome.isChecked()
-    assert not window.btnNovel.isVisible()
-    assert not window.btnCharacters.isVisible()
-    assert not window.btnScenes.isVisible()
-    assert not window.btnReport.isVisible()
-    assert not window.btnNotes.isVisible()
-    assert not window.btnTimeline.isVisible()
+    assert_views(window, visible=False)
 
 
 def test_change_font_size(qtbot, window: MainWindow):
@@ -48,33 +64,29 @@ def test_load_new_empty_novel(qtbot, filled_window: MainWindow):
 
     card.btnLoad.click()
 
-    assert filled_window.btnNovel.isEnabled()
-    assert filled_window.btnNovel.isVisible()
-    assert filled_window.btnCharacters.isVisible()
-    assert filled_window.btnCharacters.isEnabled()
-    assert filled_window.btnScenes.isVisible()
-    assert not filled_window.btnScenes.isEnabled()
-    assert filled_window.btnReport.isVisible()
-    assert not filled_window.btnReport.isEnabled()
-    assert filled_window.btnNotes.isVisible()
-    assert not filled_window.btnNotes.isEnabled()
-    assert filled_window.btnTimeline.isVisible()
-    assert not filled_window.btnTimeline.isEnabled()
-
+    assert_views(filled_window, scene_dependent_enabled=False, character_dependent_enabled=False)
     first_card = view.novel_cards[0]
     assert first_card.novel.id
     qtbot.mouseClick(first_card, Qt.LeftButton)
 
     first_card.btnLoad.click()
-    assert filled_window.btnNovel.isEnabled()
-    assert filled_window.btnNovel.isVisible()
-    assert filled_window.btnCharacters.isVisible()
-    assert filled_window.btnCharacters.isEnabled()
-    assert filled_window.btnScenes.isVisible()
-    assert filled_window.btnScenes.isEnabled()
-    assert filled_window.btnReport.isVisible()
-    assert filled_window.btnReport.isEnabled()
-    assert filled_window.btnNotes.isVisible()
-    assert filled_window.btnNotes.isEnabled()
-    assert filled_window.btnTimeline.isVisible()
-    assert filled_window.btnTimeline.isEnabled()
+    assert_views(filled_window)
+
+
+def test_import_from_scrivener(qtbot, window: MainWindow, monkeypatch):
+    folder = Path(sys.path[0]).joinpath('resources/scrivener/v3/NovelWithParts')
+    monkeypatch.setattr(QFileDialog, "getExistingDirectory", lambda *args: folder)
+
+    view: HomeView = go_to_home(window)
+    assert len(view.novel_cards) == 0
+    window.actionImportScrivener.trigger()
+
+    assert len(view.novel_cards) == 1
+
+    card = view.novel_cards[0]
+    assert card.novel.id == UUID('C4B3D990-B9C2-4FE6-861E-B06B498283A4')
+    assert card.novel.title == 'Importer project'
+    qtbot.mouseClick(card, Qt.LeftButton)
+
+    card.btnLoad.click()
+    assert_views(window)
