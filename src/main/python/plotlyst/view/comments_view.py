c7e95931c81edb017cc5d9b2a29500444d1b5aef
zkovari <zsolt@kovaridev.com>
2021-08-26 08:28:15 +0200

    Add comments view

---
+++ src/main/python/plotlyst/view/comments_view.py
@@ -0,0 +1,42 @@
+"""
+Plotlyst
+Copyright (C) 2021  Zsolt Kovari
+
+This file is part of Plotlyst.
+
+Plotlyst is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Plotlyst is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from overrides import overrides
+
+from src.main.python.plotlyst.core.domain import Novel
+from src.main.python.plotlyst.view._view import AbstractNovelView
+from src.main.python.plotlyst.view.generated.comments_view_ui import Ui_CommentsView
+from src.main.python.plotlyst.view.icons import IconRegistry
+
+
+class CommentsView(AbstractNovelView):
+
+    def __init__(self, novel: Novel):
+        super(CommentsView, self).__init__(novel)
+        self.ui = Ui_CommentsView()
+        self.ui.setupUi(self.widget)
+        self.ui.btnNewComment.setIcon(IconRegistry.from_name('mdi.comment-plus-outline'))
+        self.ui.btnNewComment.clicked.connect(self._new_comment)
+
+    @overrides
+    def refresh(self):
+        pass
+
+    def _new_comment(self):
+        pass
