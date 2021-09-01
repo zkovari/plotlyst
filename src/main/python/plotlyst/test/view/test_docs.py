1d68ce042c5e4ce2e1403a0df32e3e4a48886c9b
zkovari <zsolt@kovaridev.com>
2021-09-01 11:02:36 +0200

    test editing

--- src/main/python/plotlyst/test/view/test_docs.py
+++ src/main/python/plotlyst/test/view/test_docs.py
@@ -1,7 +1,16 @@
-from src.main.python.plotlyst.test.common import go_to_docs
+from PyQt5.QtCore import QModelIndex
+
+from src.main.python.plotlyst.core.client import json_client
+from src.main.python.plotlyst.test.common import go_to_docs, click_on_item
 from src.main.python.plotlyst.view.docs_view import DocumentsView
 from src.main.python.plotlyst.view.main_window import MainWindow
 
 
 def test_notes_display(qtbot, filled_window: MainWindow):
     view: DocumentsView = go_to_docs(filled_window)
+
+    click_on_item(qtbot, view.ui.treeDocuments, 0, 0, QModelIndex())
+    qtbot.keyClicks(view.ui.editor.textEditor, 'Test content')
+
+    json_client.load_document(view.novel, view.novel.documents[0])
+    assert 'Test content' in view.novel.documents[0].content
