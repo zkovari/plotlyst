b230f99d412605f1ecc3a31e314ec561e3aa80e3
zkovari <zsolt@kovaridev.com>
2022-02-14 15:21:14 +0100

    Persist document statistics

--- src/main/python/plotlyst/core/domain.py
+++ src/main/python/plotlyst/core/domain.py
@@ -1177,16 +1177,22 @@ class TextStatistics:
 
 
 @dataclass
+class DocumentStatistics:
+    wc: int = 0
+
+
+@dataclass
 class Document(CharacterBased):
     title: str
     id: uuid.UUID = field(default_factory=uuid.uuid4)
     type: DocumentType = DocumentType.DOCUMENT
     children: List['Document'] = field(default_factory=list)
-    character_id: Optional[uuid.UUID] = None
-    scene_id: Optional[uuid.UUID] = None
-    data_id: Optional[uuid.UUID] = None
-    icon: str = ''
-    icon_color: str = 'black'
+    character_id: Optional[uuid.UUID] = field(default=None, metadata=config(exclude=exclude_if_empty))
+    scene_id: Optional[uuid.UUID] = field(default=None, metadata=config(exclude=exclude_if_empty))
+    data_id: Optional[uuid.UUID] = field(default=None, metadata=config(exclude=exclude_if_empty))
+    icon: str = field(default='', metadata=config(exclude=exclude_if_empty))
+    icon_color: str = field(default='black', metadata=config(exclude=exclude_if_black))
+    statistics: Optional[DocumentStatistics] = field(default=None, metadata=config(exclude=exclude_if_empty))
 
     @overrides
     def __hash__(self):
