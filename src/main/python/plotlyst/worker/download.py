bd6b108f3ed333162eb326cd4e82bddfa25f7e3d
Zsolt Kovari <zsolt@kovaridev.com>
2022-03-05 16:26:15 +0100

    Fix nltk resource download on macos (#291)
    * Workaround nltk ssl problem on macos
    
    * Add custom plotlyst nltk_data to nltk path
    
    * Download punkt resource manually
    
    * check punkt resource manually
    
    * use requests to download file

--- src/main/python/plotlyst/worker/download.py
+++ src/main/python/plotlyst/worker/download.py
@@ -17,15 +17,36 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-import nltk
+import os
+import zipfile
+
+import requests
 from PyQt5.QtCore import QRunnable
 from overrides import overrides
 
 from src.main.python.plotlyst.env import app_env
 
 
+def download_file(url, target):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(target, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+
 class NltkResourceDownloadWorker(QRunnable):
 
     @overrides
     def run(self) -> None:
-        nltk.download('punkt', download_dir=app_env.nltk_data)
+        tokenizers_path = os.path.join(app_env.nltk_data, 'tokenizers')
+        if os.path.exists(os.path.join(tokenizers_path, 'punkt')):
+            print('Resource punkt is already present. Skip downloading.')
+            return
+
+        os.makedirs(tokenizers_path, exist_ok=True)
+
+        punkt_zip_path = os.path.join(tokenizers_path, 'punkt.zip')
+        download_file('https://github.com/nltk/nltk_data/raw/gh-pages/packages/tokenizers/punkt.zip', punkt_zip_path)
+        with zipfile.ZipFile(punkt_zip_path) as zip_ref:
+            zip_ref.extractall(tokenizers_path)
