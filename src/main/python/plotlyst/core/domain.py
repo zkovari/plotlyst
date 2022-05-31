2052ef78eeb56cd4a7b729bb8c386c46423644e9
zkovari <zsolt@kovaridev.com>
2022-05-31 12:19:45 +0200

    Remove story goals column and replace with summary

--- src/main/python/plotlyst/core/domain.py
+++ src/main/python/plotlyst/core/domain.py
@@ -29,7 +29,8 @@ from overrides import overrides
 
 from src.main.python.plotlyst.core.template import SelectionItem, exclude_if_empty, exclude_if_black, enneagram_field, \
     mbti_field, ProfileTemplate, default_character_profiles, default_location_profiles, protagonist_role, \
-    antagonist_role, deuteragonist_role, tertiary_role, henchmen_role, enneagram_choices, mbti_choices, Role
+    antagonist_role, deuteragonist_role, tertiary_role, henchmen_role, enneagram_choices, mbti_choices, Role, \
+    summary_field
 
 
 @dataclass
@@ -176,6 +177,13 @@ class Character:
         for value in self.template_values:
             if value.id == mbti_field.id:
                 return mbti_choices.get(value.value)
+
+    def summary(self) -> str:
+        for value in self.template_values:
+            if value.id == summary_field.id:
+                return value.value
+
+        return ''
 
     def flatten_goals(self) -> List[CharacterGoal]:
         all_goals = []
