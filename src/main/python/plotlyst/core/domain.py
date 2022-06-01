a845689afbcc3b47101721d530af72c07b5e49cf
zkovari <zsolt@kovaridev.com>
2022-06-01 09:58:27 +0200

    Change default docs

--- src/main/python/plotlyst/core/domain.py
+++ src/main/python/plotlyst/core/domain.py
@@ -939,11 +939,10 @@ class Document(CharacterBased):
 
 
 def default_documents() -> List[Document]:
-    return [Document('Story structure', id=uuid.UUID('ec2a62d9-fc00-41dd-8a6c-b121156b6cf4')),
-            Document('Characters', id=uuid.UUID('8fa16650-bed0-489b-baa1-d239e5198d47')),
-            Document('Scenes', id=uuid.UUID('75a552f4-037d-4179-860f-dd8400a7545b')),
-            Document('Worldbuilding', id=uuid.UUID('5faf7c16-f970-465d-bbcb-1bad56f3313c')),
-            Document('Brainstorming', id=uuid.UUID('f6df3a87-7054-40d6-a4b0-ad9917003136'))]
+    return [Document('Story', id=uuid.UUID('ec2a62d9-fc00-41dd-8a6c-b121156b6cf4'), icon='fa5s.book-open'),
+            Document('Characters', id=uuid.UUID('8fa16650-bed0-489b-baa1-d239e5198d47'), icon='fa5s.user'),
+            Document('Scenes', id=uuid.UUID('75a552f4-037d-4179-860f-dd8400a7545b'), icon='mdi.movie-open'),
+            Document('Locations', id=uuid.UUID('5faf7c16-f970-465d-bbcb-1bad56f3313c'), icon='fa5s.map-pin')]
 
 
 @dataclass
