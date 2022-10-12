e6d413e9693d30ec5a045f9448d71a2fe5c35668
zkovari <zsolt@kovaridev.com>
2022-10-12 11:24:50 +0200

    Separate goal fields under a new header

--- src/main/python/plotlyst/core/template.py
+++ src/main/python/plotlyst/core/template.py
@@ -470,6 +470,7 @@ def default_character_profiles() -> List[ProfileTemplate]:
 
     summary_title = TemplateField('Summary', type=TemplateFieldType.DISPLAY_HEADER, required=True)
     characterization_title = TemplateField('Personality', type=TemplateFieldType.DISPLAY_HEADER, required=True)
+    goal_title = TemplateField('Goal', type=TemplateFieldType.DISPLAY_HEADER)
     story_title = TemplateField('Story attributes', type=TemplateFieldType.DISPLAY_HEADER)
 
     fields = [ProfileElement(summary_title, 0, 0, col_span=2),
@@ -478,7 +479,7 @@ def default_character_profiles() -> List[ProfileTemplate]:
               ProfileElement(enneagram_field, 3, 0, col_span=2, margins=Margins(left=15)),
               ProfileElement(mbti_field, 4, 0, col_span=2, margins=Margins(left=15)),
               ProfileElement(traits_field, 5, 0, col_span=2, margins=Margins(left=15)),
-              ProfileElement(story_title, 6, 0, col_span=2),
+              ProfileElement(goal_title, 6, 0, col_span=2),
               ProfileElement(goal_field, 7, 0, margins=Margins(left=15)),
               ProfileElement(internal_goal_field, 7, 1, margins=Margins(left=10)),
               ProfileElement(arrow_field(), 8, 0),
@@ -493,10 +494,11 @@ def default_character_profiles() -> List[ProfileTemplate]:
               ProfileElement(internal_arrow_field(), 12, 1),
               ProfileElement(stakes_field, 13, 0, margins=Margins(left=15)),
               ProfileElement(internal_stakes_field, 13, 1, margins=Margins(left=10)),
-              ProfileElement(need_field, 14, 0, col_span=2, margins=Margins(left=15)),
-              ProfileElement(weaknesses_field, 15, 0, col_span=2, margins=Margins(left=15)),
-              ProfileElement(ghost_field, 16, 0, col_span=2, margins=Margins(left=15)),
-              ProfileElement(values_field, 17, 0, col_span=2, margins=Margins(left=15))
+              ProfileElement(story_title, 14, 0, col_span=2),
+              ProfileElement(need_field, 15, 0, col_span=2, margins=Margins(left=15)),
+              ProfileElement(weaknesses_field, 16, 0, col_span=2, margins=Margins(left=15)),
+              ProfileElement(ghost_field, 17, 0, col_span=2, margins=Margins(left=15)),
+              ProfileElement(values_field, 18, 0, col_span=2, margins=Margins(left=15))
               ]
     return [ProfileTemplate(title='Default character template',
                             id=uuid.UUID('6e89c683-c132-469b-a75c-6712af7c339d'),
