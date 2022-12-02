54fb7c8fb7b086501129b6f28d4c84f3bff30fba
zkovari <zsolt@kovaridev.com>
2022-12-02 10:59:26 +0100

    Fix missing color for act buttons

--- src/main/python/plotlyst/common.py
+++ src/main/python/plotlyst/common.py
@@ -24,11 +24,11 @@ EXIT_CODE_RESTART = 10
 WIP_COLOR: str = '#f6cd61'
 PIVOTAL_COLOR: str = '#3da4ab'
 
-ACT_ONE_COLOR: str = 'rgb(2, 188, 212)'
+ACT_ONE_COLOR: str = '#02bcd4'
 TRANS_ACT_ONE_COLOR: str = 'rgba(2, 188, 212, 45)'
-ACT_TWO_COLOR: str = 'rgb(27, 188, 156)'
+ACT_TWO_COLOR: str = '#1bbc9c'
 TRANS_ACT_TWO_COLOR: str = 'rgba(27, 188, 156, 45)'
-ACT_THREE_COLOR: str = 'rgb(255, 120, 0)'
+ACT_THREE_COLOR: str = '#ff7800'
 TRANS_ACT_THREE_COLOR: str = 'rgba(255, 120, 0, 45)'
 
 CONFLICT_CHARACTER_COLOR: str = '#c1666b'
