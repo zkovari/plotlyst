dc724266a00027eacb7cd5a492b41dcc0cd1d89e
Zsolt Kovari <kovari.zsolt91@gmail.com>
2023-03-04 10:20:53 +0100

    Remove custom qt-handy installation

--- update_plotlyst_libs.sh
+++ update_plotlyst_libs.sh
@@ -6,7 +6,6 @@ set -e
 pip uninstall -y qt-uic
 pip uninstall -y qt-anim
 pip uninstall -y qt-textedit
-pip uninstall -y qt-handy
 pip uninstall -y qt-emojipicker
 pip uninstall -y qt-frames
 pip uninstall -y qt-toolbox
@@ -14,7 +13,6 @@ pip uninstall -y qt-toolbox
 pip install git+https://github.com/plotlyst/qt-uic.git
 pip install git+https://github.com/plotlyst/qt-anim.git
 pip install git+https://github.com/plotlyst/qt-textedit.git
-pip install git+https://github.com/plotlyst/qt-handy.git
 pip install git+https://github.com/plotlyst/qt-emojipicker.git
 pip install git+https://github.com/plotlyst/qt-frames.git
 pip install git+https://github.com/plotlyst/qt-toolbox-widget.git
