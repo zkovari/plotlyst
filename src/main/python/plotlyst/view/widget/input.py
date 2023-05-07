627cfd0e1d116c54b3f3f80f478cba568a203379
zkovari <zsolt@kovaridev.com>
2023-05-07 18:08:48 +0200

    Apply margins for docs

--- src/main/python/plotlyst/view/widget/input.py
+++ src/main/python/plotlyst/view/widget/input.py
@@ -436,6 +436,7 @@ class DocumentTextEditor(RichTextEditor):
         self.setProperty('relaxed-white-bg', True)
 
         self.textEdit.setViewportMargins(5, 5, 5, 5)
+        margins(self, top=50, right=10)
 
         self.highlighter = self._initHighlighter()
 
