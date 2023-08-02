563c67d42acfec5dce98413549d78c0729800daf
Zsolt Kovari <kovari.zsolt91@gmail.com>
2023-08-02 08:36:09 +0200

    Scene synopsis editor in manuscript view

---
+++ src/main/python/plotlyst/view/widget/scene/editor.py
@@ -0,0 +1,119 @@
+"""
+Plotlyst
+Copyright (C) 2021-2023  Zsolt Kovari
+
+This file is part of Plotlyst.
+
+Plotlyst is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Plotlyst is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from functools import partial
+from typing import List, Optional
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QWidget, QTextEdit, QPushButton, QLabel
+from overrides import overrides
+from qthandy import vbox, vspacer, transparent, sp, line, incr_font
+from qtmenu import MenuWidget
+
+from src.main.python.plotlyst.core.domain import Scene, Novel
+from src.main.python.plotlyst.event.core import emit_event, EventListener, Event
+from src.main.python.plotlyst.event.handler import event_dispatcher
+from src.main.python.plotlyst.events import SceneChangedEvent
+from src.main.python.plotlyst.service.persistence import RepositoryPersistenceManager
+from src.main.python.plotlyst.view.common import DelayedSignalSlotConnector, action
+
+
+class SceneMiniEditor(QWidget, EventListener):
+
+    def __init__(self, novel: Novel, parent=None):
+        super().__init__(parent)
+        self._novel = novel
+        self._scenes: List[Scene] = []
+        self._currentScene: Optional[Scene] = None
+
+        self._lblScene = QLabel()
+        incr_font(self._lblScene, 2)
+        self._btnScenes = QPushButton()
+        incr_font(self._btnScenes, 2)
+        transparent(self._btnScenes)
+        sp(self._btnScenes).h_exp()
+        self._menuScenes = MenuWidget(self._btnScenes)
+
+        self._textSynopsis = QTextEdit()
+        self._textSynopsis.setProperty('white-bg', True)
+        self._textSynopsis.setProperty('rounded', True)
+        self._textSynopsis.setPlaceholderText('Write a short summary of this scene')
+        self._textSynopsis.setMaximumHeight(200)
+
+        self._layout = vbox(self)
+        self._layout.addWidget(self._btnScenes, alignment=Qt.AlignmentFlag.AlignCenter)
+        self._layout.addWidget(self._lblScene, alignment=Qt.AlignmentFlag.AlignCenter)
+        self._layout.addWidget(line())
+        self._layout.addWidget(QLabel('Synopsis:'), alignment=Qt.AlignmentFlag.AlignLeft)
+        self._layout.addWidget(self._textSynopsis)
+        self._layout.addWidget(vspacer())
+
+        DelayedSignalSlotConnector(self._textSynopsis.textChanged, self._save, parent=self)
+
+        self._repo = RepositoryPersistenceManager.instance()
+        event_dispatcher.register(self, SceneChangedEvent)
+
+    def setScene(self, scene: Scene):
+        self.setScenes([scene])
+
+    def setScenes(self, scenes: List[Scene]):
+        self.reset()
+        self._scenes.extend(scenes)
+
+        if len(self._scenes) > 1:
+            for scene in scenes:
+                self._menuScenes.addAction(action(
+                    scene.title_or_index(self._novel), slot=partial(self.selectScene, scene)
+                ))
+
+        self._lblScene.setVisible(len(self._scenes) == 1)
+        self._btnScenes.setVisible(len(self._scenes) > 1)
+
+        if self._scenes:
+            self.selectScene(self._scenes[0])
+
+    def selectScene(self, scene: Scene):
+        self._save()
+        self._currentScene = None
+        if len(self._scenes) > 1:
+            self._btnScenes.setText(scene.title_or_index(self._novel))
+        else:
+            self._lblScene.setText(scene.title_or_index(self._novel))
+        self._textSynopsis.setText(scene.synopsis)
+        self._currentScene = scene
+
+    def reset(self):
+        self._save()
+        self._currentScene = None
+        self._scenes.clear()
+        self._btnScenes.setText('')
+        self._menuScenes.clear()
+        self._textSynopsis.clear()
+
+    @overrides
+    def event_received(self, event: Event):
+        if isinstance(event, SceneChangedEvent):
+            if event.scene is self._currentScene:
+                self.selectScene(self._currentScene)
+
+    def _save(self):
+        if self._currentScene and self._currentScene.synopsis != self._textSynopsis.toPlainText():
+            self._currentScene.synopsis = self._textSynopsis.toPlainText()
+            self._repo.update_scene(self._currentScene)
+            emit_event(SceneChangedEvent(self, self._currentScene))
