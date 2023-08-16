fd97e2d205bebbab83ce412d6cb2a3e275b01458
Zsolt Kovari <kovari.zsolt91@gmail.com>
2023-08-16 09:57:11 +0200

    Start eventitem

--- src/main/python/plotlyst/view/widget/events_map.py
+++ src/main/python/plotlyst/view/widget/events_map.py
@@ -17,16 +17,19 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+from enum import Enum
 from typing import Optional
 
-from PyQt6.QtCore import QRectF, Qt, QPointF, pyqtSignal
-from PyQt6.QtGui import QColor, QPainter, QPen, QKeyEvent
+from PyQt6.QtCore import QRectF, Qt, QPointF, pyqtSignal, QRect, QPoint
+from PyQt6.QtGui import QColor, QPainter, QPen, QKeyEvent, QFontMetrics
 from PyQt6.QtWidgets import QGraphicsScene, QWidget, QAbstractGraphicsShapeItem, QGraphicsSceneHoverEvent, \
-    QGraphicsSceneMouseEvent, QStyleOptionGraphicsItem
+    QGraphicsSceneMouseEvent, QStyleOptionGraphicsItem, QGraphicsTextItem, QApplication
 from overrides import overrides
+from qtmenu import MenuWidget
 
 from src.main.python.plotlyst.common import RELAXED_WHITE_COLOR, PLOTLYST_SECONDARY_COLOR
-from src.main.python.plotlyst.core.domain import Novel, Character, CharacterNode
+from src.main.python.plotlyst.core.domain import Novel, Character, CharacterNode, Node
+from src.main.python.plotlyst.view.common import action
 from src.main.python.plotlyst.view.icons import avatars
 from src.main.python.plotlyst.view.widget.graphics import BaseGraphicsView, NodeItem, ConnectorItem
 
@@ -34,6 +37,26 @@ from src.main.python.plotlyst.view.widget.graphics import BaseGraphicsView, Node
 def draw_rect(painter: QPainter, item: QAbstractGraphicsShapeItem):
     painter.setPen(QPen(Qt.GlobalColor.red, 1, Qt.PenStyle.DashLine))
     painter.drawRoundedRect(item.boundingRect(), 2, 2)
+
+
+def draw_center(painter: QPainter, item: QAbstractGraphicsShapeItem):
+    painter.setPen(QPen(Qt.GlobalColor.red, 1, Qt.PenStyle.DashLine))
+    painter.drawEllipse(item.boundingRect().center(), 1, 1)
+
+
+def draw_zero(painter: QPainter):
+    painter.setPen(QPen(Qt.GlobalColor.blue, 1, Qt.PenStyle.DashLine))
+    painter.drawEllipse(QPointF(0, 0), 1, 1)
+
+
+def draw_helpers(painter: QPainter, item: QAbstractGraphicsShapeItem):
+    draw_rect(painter, item)
+    draw_center(painter, item)
+    draw_zero(painter)
+
+
+class ItemType(Enum):
+    Event = 0
 
 
 class SocketItem(QAbstractGraphicsShapeItem):
@@ -86,6 +109,57 @@ class PlaceholderItem(SocketItem):
         self.setToolTip('Click to add a new node')
 
 
+class EventItem(NodeItem):
+    def __init__(self, node: Node, parent=None):
+        super().__init__(node, parent)
+        self._text: str = 'Type'
+        self.setPos(node.x, node.y)
+
+        self._margin = 30
+        self._padding = 20
+
+        self._metrics = QFontMetrics(QApplication.font())
+        self._textRect: QRect = QRect(0, 0, 1, 1)
+        self._width = 1
+        self._height = 1
+        self._nestedRectWidth = 1
+        self._nestedRectHeight = 1
+
+        self._recalculateRect()
+
+    def text(self) -> str:
+        return self._text
+
+    def setText(self, text: str):
+        self._text = text
+        self._recalculateRect()
+
+    @overrides
+    def boundingRect(self) -> QRectF:
+        return QRectF(0, 0, self._width, self._height)
+
+    @overrides
+    def paint(self, painter: QPainter, option: 'QStyleOptionGraphicsItem', widget: Optional[QWidget] = ...) -> None:
+        if self.isSelected():
+            painter.setPen(QPen(Qt.GlobalColor.gray, 2, Qt.PenStyle.DashLine))
+            painter.drawRoundedRect(self._margin, self._margin, self._nestedRectWidth, self._nestedRectHeight, 2, 2)
+
+        painter.setPen(QPen(Qt.GlobalColor.black, 1))
+        painter.drawText(self._textRect, Qt.AlignmentFlag.AlignCenter, self._text)
+        painter.drawRoundedRect(self._margin, self._margin, self._nestedRectWidth, self._nestedRectHeight, 18, 18)
+
+        draw_helpers(painter, self)
+
+    def _recalculateRect(self):
+        self._textRect = self._metrics.boundingRect(self._text)
+        self._textRect.moveTopLeft(QPoint(self._margin + self._padding, self._margin + self._padding))
+        self._width = self._textRect.width() + self._margin * 2 + self._padding * 2
+        self._height = self._textRect.height() + self._margin * 2 + self._padding * 2
+
+        self._nestedRectWidth = self._textRect.width() + self._padding * 2
+        self._nestedRectHeight = self._textRect.height() + self._padding * 2
+
+
 class CharacterItem(NodeItem):
     def __init__(self, character: Character, node: CharacterNode, parent=None):
         super().__init__(node, parent)
@@ -114,13 +188,16 @@ class CharacterItem(NodeItem):
     def boundingRect(self) -> QRectF:
         return QRectF(0, 0, self._size + self._margin * 2, self._size + self._margin * 2)
 
+    def rightSocket(self) -> SocketItem:
+        return self._socketRight
+
     @overrides
     def paint(self, painter: QPainter, option: 'QStyleOptionGraphicsItem', widget: Optional[QWidget] = ...) -> None:
         if self.isSelected():
             painter.setPen(QPen(Qt.GlobalColor.gray, 2, Qt.PenStyle.DashLine))
             painter.drawRoundedRect(self._margin, self._margin, self._size, self._size, 2, 2)
 
-        draw_rect(painter, self)
+        draw_helpers(painter, self)
 
         avatar = avatars.avatar(self._character)
         avatar.paint(painter, self._margin, self._margin, self._size, self._size)
@@ -140,9 +217,14 @@ class EventsMindMapScene(QGraphicsScene):
         self._placeholder: Optional[PlaceholderItem] = None
         self._connector: Optional[ConnectorItem] = None
 
-        item = CharacterItem(novel.characters[0], CharacterNode(50, 50))
-        item.setPos(50, 50)
-        self.addItem(item)
+        characterItem = CharacterItem(novel.characters[0], CharacterNode(50, 50))
+        characterItem.setPos(50, 50)
+
+        eventItem = EventItem(Node(400, 100))
+        connector = ConnectorItem(characterItem.rightSocket(), eventItem)
+        self.addItem(characterItem)
+        self.addItem(eventItem)
+        self.addItem(connector)
 
     def linkMode(self) -> bool:
         return self._linkMode
@@ -164,6 +246,19 @@ class EventsMindMapScene(QGraphicsScene):
         self._connector = None
         self._placeholder = None
 
+    def addNewItem(self, pos: QPointF, itemType: ItemType):
+        if itemType == ItemType.Event:
+            item = QGraphicsTextItem('Type')
+        else:
+            return
+
+        item.setPos(pos)
+        connector = ConnectorItem(self._connector.source(), item)
+        self.addItem(item)
+        self.addItem(connector)
+
+        self.endLink()
+
     @overrides
     def mouseMoveEvent(self, event: 'QGraphicsSceneMouseEvent') -> None:
         if self.linkMode():
@@ -184,9 +279,6 @@ class EventsMindMapScene(QGraphicsScene):
             else:
                 print('add new node')
                 self.addNewNode.emit(self._placeholder)
-            # view_pos = self.mapFromScene(item.sceneBoundingRect().topRight())
-            # self._itemEditor.edit(item, newItem, self.mapToGlobal(view_pos))
-                self.endLink()
         super().mouseReleaseEvent(event)
 
 
@@ -199,4 +291,15 @@ class EventsMindMapView(BaseGraphicsView):
         self.setScene(self._scene)
         self.setBackgroundBrush(QColor(RELAXED_WHITE_COLOR))
 
-        self.scale(0.6, 0.6)
+        # self.scale(0.6, 0.6)
+
+        self._scene.addNewNode.connect(self._displayNewNodeMenu)
+
+    def _displayNewNodeMenu(self, placeholder: PlaceholderItem):
+        view_pos = self.mapFromScene(placeholder.sceneBoundingRect().center())
+        menu = MenuWidget(self)
+        menu.addAction(
+            action('Event',
+                   slot=lambda: self._scene.addNewItem(placeholder.sceneBoundingRect().center(), ItemType.Event)))
+        menu.exec(self.mapToGlobal(view_pos))
+        # self._itemEditor.edit(item, newItem, self.mapToGlobal(view_pos))
