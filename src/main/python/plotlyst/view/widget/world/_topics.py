0aa081d37a9de38de0bbbced3cdf8a49bec42615
Zsolt Kovari <kovari.zsolt91@gmail.com>
2024-01-04 08:43:47 +0100

    topics

--- src/main/python/plotlyst/view/widget/world/_topics.py
+++ src/main/python/plotlyst/view/widget/world/_topics.py
@@ -79,19 +79,52 @@ infrastructural_topics = [
     Topic('Healthcare', TopicType.Worldbuilding, uuid.UUID('34c30a3d-4b35-4999-a2f5-f8d0e74f4f58'),
           icon='mdi.hospital-box')
 ]
-religious_topics = []
-fantastic_topics = []
-nefarious_topics = []
-environmental_topics = []
-
-Topic('Gods', TopicType.Worldbuilding, uuid.UUID('e842e5de-2795-4780-bf5d-c3d06e2893c6'), icon='ri.thunderstorms-fill')
-Topic('Religions', TopicType.Worldbuilding, uuid.UUID('9e276172-2e47-4ba5-b7a2-bf2da6cb4481'), icon='fa5s.church')
-Topic('Afterlife', TopicType.Worldbuilding, uuid.UUID('b8bda0ad-f9f7-4e7b-873c-121b4157e4a4'), icon='')
-Topic('', TopicType.Worldbuilding, uuid.UUID('a63ebf22-7323-489c-a3ac-9048c82905c8'), icon='')
-Topic('', TopicType.Worldbuilding, uuid.UUID('6a1b6516-b1ba-4215-9fc9-f7d2d1acc71a'), icon='')
-Topic('', TopicType.Worldbuilding, uuid.UUID('213fab5e-a522-4e6a-ac03-0ae851706404'), icon='')
-Topic('', TopicType.Worldbuilding, uuid.UUID('8d25f651-dd2e-4d34-814e-09b5c91cd26b'), icon='')
-Topic('', TopicType.Worldbuilding, uuid.UUID('562efc59-4fdf-4ebb-ab64-b25555658402'), icon='')
-Topic('', TopicType.Worldbuilding, uuid.UUID('e6303c7c-0ba7-4ea8-9c45-b37d9dba8469'), icon='')
-Topic('', TopicType.Worldbuilding, uuid.UUID('23604779-52e0-43c7-96b9-bbd08cde8ade'), icon='')
-Topic('', TopicType.Worldbuilding, uuid.UUID('afa517d6-d964-4514-b4b1-7673f7b5c969'), icon='')
+religious_topics = [
+    Topic('Gods', TopicType.Worldbuilding, uuid.UUID('e842e5de-2795-4780-bf5d-c3d06e2893c6'),
+          icon='ri.thunderstorms-fill'),
+    Topic('Religions', TopicType.Worldbuilding, uuid.UUID('9e276172-2e47-4ba5-b7a2-bf2da6cb4481'), icon='fa5s.church'),
+    Topic('Sacred texts', TopicType.Worldbuilding, uuid.UUID('a63ebf22-7323-489c-a3ac-9048c82905c8'),
+          icon='fa5s.scroll'),
+    Topic('Prophecies', TopicType.Worldbuilding, uuid.UUID('6a1b6516-b1ba-4215-9fc9-f7d2d1acc71a'),
+          icon='mdi6.crystal-ball')
+]
+fantastic_topics = [
+    Topic('Magic', TopicType.Worldbuilding, uuid.UUID('562efc59-4fdf-4ebb-ab64-b25555658402'), icon='fa5s.magic'),
+    Topic('Constructs', TopicType.Worldbuilding, uuid.UUID('e6303c7c-0ba7-4ea8-9c45-b37d9dba8469'), icon='mdi.robot'),
+    Topic('Paranormal', TopicType.Worldbuilding, uuid.UUID('23604779-52e0-43c7-96b9-bbd08cde8ade'), icon='mdi6.ghost'),
+    Topic('Aliens', TopicType.Worldbuilding, uuid.UUID('8d25f651-dd2e-4d34-814e-09b5c91cd26b'), icon='mdi.alien'),
+    Topic('Magical creatures', TopicType.Worldbuilding, uuid.UUID('213fab5e-a522-4e6a-ac03-0ae851706404'),
+          icon='fa5s.dragon'),
+    Topic('Legendary items', TopicType.Worldbuilding, uuid.UUID('afa517d6-d964-4514-b4b1-7673f7b5c969'),
+          icon='mdi6.axe-battle')
+]
+nefarious_topics = [
+    Topic('Outlaws', TopicType.Worldbuilding, uuid.UUID('f6768a15-6b68-474b-a6db-982ffbb5b6f0'), icon='mdi.robber'),
+    Topic('Crime', TopicType.Worldbuilding, uuid.UUID('add66039-19b6-4534-afcf-adf85a7f36ff'), icon='mdi.knife'),
+    Topic('Cults', TopicType.Worldbuilding, uuid.UUID('5a114e40-6f9e-4c1c-9442-b337e9af5497'), icon='mdi.pentagram'),
+    Topic('Corruption', TopicType.Worldbuilding, uuid.UUID('4519bef9-accd-4e27-865c-7e320fb6c2f3'), icon='mdi.sack'),
+    Topic('Disease', TopicType.Worldbuilding, uuid.UUID('72f2c4b6-80a1-41ec-bc2a-984598868e21'), icon='mdi.virus'),
+    Topic('Espionage', TopicType.Worldbuilding, uuid.UUID('79218b0d-1c30-4a41-9b4b-46ebe2b19e6b'), icon='ri.spy-fill'),
+    Topic('Curses', TopicType.Worldbuilding, uuid.UUID('f4030cbf-28e6-4a57-95a5-b6d12d4d2ed9'), icon='fa5s.skull'),
+    Topic('Forbidden tech', TopicType.Worldbuilding, uuid.UUID('da24909b-83b1-4f67-b841-0e0293c9d97c'),
+          icon='fa5s.biohazard'),
+]
+environmental_topics = [
+    Topic('Forests', TopicType.Worldbuilding, uuid.UUID('8c3526fc-daa5-47c9-888c-010f1bd475f8'), icon='mdi6.forest'),
+    Topic('Mountains', TopicType.Worldbuilding, uuid.UUID('0a0a254c-c2fe-4235-94ec-41d9d3715bd0'),
+          icon='fa5s.mountain'),
+    Topic('Weather', TopicType.Worldbuilding, uuid.UUID('2fe12691-ce5b-42d6-b055-8e6472e09714'),
+          icon='mdi.weather-lightning-rainy'),
+    Topic('Natural hazards', TopicType.Worldbuilding, uuid.UUID('929b4a0a-4a59-4bc4-aa58-c9dc643b7eb5'),
+          icon='mdi.weather-hurricane'),
+    Topic('Islands', TopicType.Worldbuilding, uuid.UUID('95a06aef-3cda-4d11-81ca-e4b5134c7df5'), icon='mdi.island'),
+    Topic('Terrain', TopicType.Worldbuilding, uuid.UUID('b2c2c6c4-3475-4219-bbdd-ddcee2ba4e41'), icon='mdi.terrain'),
+    Topic('Planets', TopicType.Worldbuilding, uuid.UUID('d8c78689-2730-4127-9565-b6f75c98e362'), icon='ph.planet-fill'),
+    Topic('Universum', TopicType.Worldbuilding, uuid.UUID('83d74ed5-f9e3-4a2d-aa18-d92632da9b78'),
+          icon='mdi.atom-variant'),
+    Topic('Moons', TopicType.Worldbuilding, uuid.UUID('ba829ae8-4563-42fe-9ffd-50737db617b6'), icon='fa5.moon'),
+    Topic('Stars', TopicType.Worldbuilding, uuid.UUID('e45de6ad-07e5-4c78-8246-f715b6699c50'),
+          icon='mdi.star-four-points'),
+    Topic('Black holes', TopicType.Worldbuilding, uuid.UUID('b9720d11-51d9-4ccc-bee9-bbb2229c9d18'),
+          icon='fa5s.circle'),
+]
