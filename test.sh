95fb7cd555137a3645ba790e9bd493f03ac7f25d
Zsolt Kovari <kovari.zsolt91@gmail.com>
2024-01-30 14:34:09 +0100

    Add path to tests

--- test.sh
+++ test.sh
@@ -6,4 +6,5 @@ set -e
 # generate UI > Python code first
 ./gen.sh
 export PLOTLYST_TEST_ENV=1
+export PYTHONPATH=src/main/python
 python -X faulthandler -m pytest src/main/python/plotlyst  --cov=src.main.python.plotlyst --junitxml=report.xml --cov-report html:coverage --cov-report term -v --color=yes
