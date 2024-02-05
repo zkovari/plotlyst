83cf6227010a15e02698abc01478c5f13231f1a1
Zsolt Kovari <kovari.zsolt91@gmail.com>
2024-02-05 15:13:44 +0100

    fix coverage package

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ set -e
 ./gen.sh
 export PLOTLYST_TEST_ENV=1
 export PYTHONPATH=src/main/python
-python -X faulthandler -m pytest src/main/python/plotlyst  --cov=src.main.python.plotlyst --junitxml=report.xml --cov-report html:coverage --cov-report term -v --color=yes
+python -X faulthandler -m pytest src/main/python/plotlyst  --cov=plotlyst --junitxml=report.xml --cov-report html:coverage --cov-report term -v --color=yes
