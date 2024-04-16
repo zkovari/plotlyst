946eb53049135569ddfe1ea399992a73755b41f4
Zsolt Kovari <kovari.zsolt91@gmail.com>
2024-04-16 14:35:02 +0200

    use container wdg

--- src/main/python/plotlyst/view/widget/character/profile.py
+++ src/main/python/plotlyst/view/widget/character/profile.py
@@ -17,18 +17,17 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from functools import partial
 from typing import Optional, List, Dict
 
 from PyQt6.QtCore import pyqtSignal, Qt, QSize
 from PyQt6.QtGui import QResizeEvent
 from PyQt6.QtWidgets import QWidget
 from overrides import overrides
-from qthandy import vbox, clear_layout, hbox, bold, underline, spacer, vspacer
+from qthandy import vbox, clear_layout, hbox, bold, underline, spacer, vspacer, margins
 
 from plotlyst.core.domain import Character, CharacterProfileSectionType, CharacterProfileSectionReference, \
     CharacterProfileFieldReference, CharacterProfileFieldType
-from plotlyst.view.common import tool_btn, label, wrap
+from plotlyst.view.common import tool_btn, label
 from plotlyst.view.icons import IconRegistry
 from plotlyst.view.widget.button import CollapseButton
 from plotlyst.view.widget.progress import CircularProgressBar
@@ -45,18 +44,27 @@ class ProfileSectionWidget(ProfileFieldWidget):
     def __init__(self, section: CharacterProfileSectionReference, parent=None):
         super().__init__(parent)
         self.section = section
-        hbox(self, margin=1, spacing=0)
+        vbox(self)
         self.btnHeader = CollapseButton(Qt.Edge.BottomEdge, Qt.Edge.RightEdge)
         self.btnHeader.setIconSize(QSize(16, 16))
         bold(self.btnHeader)
         underline(self.btnHeader)
         self.btnHeader.setText(section.type.name)
         self.btnHeader.setToolTip(section.type.name)
-        self.layout().addWidget(self.btnHeader)
 
+        self.wdgHeader = QWidget()
+        hbox(self.wdgHeader, 1, 0)
+        self.wdgHeader.layout().addWidget(self.btnHeader)
         self.progress = CircularProgressBar()
-        self.layout().addWidget(self.progress, alignment=Qt.AlignmentFlag.AlignVCenter)
-        self.layout().addWidget(spacer())
+        self.wdgHeader.layout().addWidget(self.progress, alignment=Qt.AlignmentFlag.AlignVCenter)
+        self.wdgHeader.layout().addWidget(spacer())
+
+        self.wdgContainer = QWidget()
+        vbox(self.wdgContainer, 0)
+        margins(self.wdgContainer, left=20)
+
+        self.layout().addWidget(self.wdgHeader)
+        self.layout().addWidget(self.wdgContainer)
 
         self.children: List[ProfileFieldWidget] = []
         self.progressStatuses: Dict[ProfileFieldWidget, float] = {}
@@ -65,6 +73,7 @@ class ProfileSectionWidget(ProfileFieldWidget):
 
     def attachWidget(self, widget: ProfileFieldWidget):
         self.children.append(widget)
+        self.wdgContainer.layout().addWidget(widget)
         # if not widget.field.type.is_display():
         self.progressStatuses[widget] = False
         # widget.valueFilled.connect(partial(self._valueFilled, widget))
@@ -78,8 +87,7 @@ class ProfileSectionWidget(ProfileFieldWidget):
         self.btnHeader.setChecked(collapsed)
 
     def _toggleCollapse(self, checked: bool):
-        for wdg in self.children:
-            wdg.setHidden(checked)
+        self.wdgContainer.setHidden(checked)
 
     def _valueFilled(self, widget: ProfileFieldWidget, value: float):
         if self.progressStatuses[widget] == value:
@@ -154,7 +162,6 @@ class CharacterProfileEditor(QWidget):
             self.layout().addWidget(wdg)
             for field in section.fields:
                 fieldWdg = label(field.type.name)
-                self.layout().addWidget(wrap(fieldWdg, margin_left=20))
                 wdg.attachWidget(fieldWdg)
 
         self.layout().addWidget(vspacer())
