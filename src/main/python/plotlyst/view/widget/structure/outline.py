bfd331b167bb112e4fa00f2ff74031923770442f
Zsolt Kovari <kovari.zsolt91@gmail.com>
2024-07-22 13:56:49 +0200

    remove pps

--- src/main/python/plotlyst/view/widget/structure/outline.py
+++ src/main/python/plotlyst/view/widget/structure/outline.py
@@ -36,7 +36,7 @@ from plotlyst.core.domain import StoryBeat, StoryBeatType, midpoints, hook_beat,
     disturbance_beat, characteristic_moment_beat, normal_world_beat, general_beat, StoryStructure, turn_beat, \
     twist_beat, inciting_incident_beat, refusal_beat, synchronicity_beat, establish_beat, trigger_beat, \
     first_pinch_point_beat, second_pinch_point_beat, crisis, climax_beat, resolution_beat, contrast_beat, \
-    retrospection_beat, revelation_beat, first_plot_point, second_plot_point, first_plot_point_ponr
+    retrospection_beat, revelation_beat
 from plotlyst.view.common import label, push_btn, wrap, tool_btn, scrolled
 from plotlyst.view.icons import IconRegistry
 from plotlyst.view.layout import group
