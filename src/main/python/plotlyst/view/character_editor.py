50a79be6bbdc9749ef4abb7160a450a3aff89dfb
Zsolt Kovari <zsolt@kovaridev.com>
2024-08-29 09:18:54 +0200

    Remove whitebg from character editor on mac

--- src/main/python/plotlyst/view/character_editor.py
+++ src/main/python/plotlyst/view/character_editor.py
@@ -24,7 +24,7 @@ import qtanim
 from PyQt6.QtCore import pyqtSignal, QObject, QTimer
 from PyQt6.QtWidgets import QWidget, QAbstractButton, QLineEdit, QCompleter
 from overrides import overrides
-from qthandy import translucent, btn_popup, bold, italic, incr_font, margins
+from qthandy import translucent, bold, italic, incr_font, margins
 from qthandy.filter import OpacityEventFilter
 from qtmenu import MenuWidget
 
@@ -32,12 +32,14 @@ from plotlyst.common import PLOTLYST_SECONDARY_COLOR
 from plotlyst.core.client import json_client
 from plotlyst.core.domain import Novel, Character, Document, FEMALE, SelectionItem
 from plotlyst.core.template import protagonist_role
+from plotlyst.env import app_env
 from plotlyst.event.core import EventListener, Event
 from plotlyst.event.handler import event_dispatchers, global_event_dispatcher
 from plotlyst.events import NovelAboutToSyncEvent
 from plotlyst.resources import resource_registry
 from plotlyst.service.persistence import RepositoryPersistenceManager
 from plotlyst.service.tour import TourService
+from plotlyst.view.common import emoji_font, set_tab_icon, ButtonPressResizeEventFilter, set_tab_visible
 from plotlyst.view.generated.character_editor_ui import Ui_CharacterEditor
 from plotlyst.view.icons import IconRegistry
 from plotlyst.view.style.base import apply_bg_image, apply_white_menu
@@ -157,8 +159,8 @@ class CharacterEditor(QObject, EventListener):
         # self.ui.tabTopics.layout().addWidget(self.wdgTopicsEditor)
 
         self.profile = CharacterProfileEditor(self.novel)
-        # if app_env.is_windows() or app_env.is_linux():
-        self.ui.wdgProfile.setProperty('relaxed-white-bg', True)
+        if app_env.is_windows() or app_env.is_linux():
+            self.ui.wdgProfile.setProperty('relaxed-white-bg', True)
         margins(self.ui.wdgTop, bottom=15)
         self.ui.wdgProfile.layout().addWidget(self.profile)
 
