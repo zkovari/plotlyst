ba6ac2c5b965ed3f1c1a4bd84bf6a0a58e4bc3b6
Zsolt Kovari <kovari.zsolt91@gmail.com>
2025-01-06 10:10:01 +0100

    Update version

--- src/main/python/plotlyst/version.py
+++ src/main/python/plotlyst/version.py
@@ -18,5 +18,5 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-plotlyst_product_version = '0.4.0'
-plotlyst_display_version = '2024.12'
+plotlyst_product_version = '0.5.0'
+plotlyst_display_version = '2025.01'
